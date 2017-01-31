8f25726b0fbb7202c62bcc21ecfa9c0d6bbb6bd4
dcalderon16 <dcalderon14@nycboss.org>
2017-01-31 17:50:39 -0500

    week1

---
+++ week1/js/sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+    createCanvas(800,800)
+
+    line(15, 25, 70, 90);
+    //head
+    ellipse(300,300,600);
+    //eyes
+    ellipse(150,230,150,75);
+    ellipse(450,230,150,75);
+}
