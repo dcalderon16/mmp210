2b228bc3e8ddd5517c8bda6f3108f71c48437c08
dcalderon16 <dcalderon14@nycboss.org>
2017-02-07 17:31:02 -0500

    Add files via upload

---
+++ week1/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(640,480);
+    background('green');
+    ellipse(50,50,50);
+    arc(150,50,50,75,0,PI+1);
+    line(50,50,150,50);
+    quad(75,75,150,150,80,180,50,65);
+}
