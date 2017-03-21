2dc1b37b9454a75578f71c985e95cc934be82442
dcalderon16 <dcalderon14@nycboss.org>
2017-03-21 17:04:22 -0400

    Add files via upload

---
+++ mmp210calderon/week1/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(640,480);
+    background('black');
+    //nose
+    ellipse(320,240,50);
+    stroke('white');
+    //whiskers
+    line(270,240,150,240);
+    line(370,240,490,240);
+    line(270,250,150,300);
+    line(370,250,490,300);
+    line(270,230,150,180);
+    line(370,230,490,180);
+    
+}
---
+++ mmp210calderon/week2/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(640, 480)
+    
+    var blue = color(255, 204, 51);
+    var purple = color(138, 0, 184);
+
+    ellipse(320,50,80);
+    
+    fill(blue);
+    ellipse(320,240,50, 350);
+
+    fill(purple);
+    ellipse(320,50,80);
+
+
+}
