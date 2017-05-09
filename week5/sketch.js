b68b257ce5c5bda2eaac38769eb82b275fdb371c
dcalderon16 <dcalderon14@nycboss.org>
2017-05-09 17:26:25 -0400

    Add files via upload

--- week5/sketch.js
+++ week5/sketch.js
@@ -1,16 +1,30 @@
 function setup() { 
-  createCanvas(600, 600);
-  background(110);
-  
-  var space = 50;
+   createCanvas(640, 480);
+    noStroke();
+}
 
-  for (var x = space/9; x < width; x+=space) {
-    for (var y = space/9; y < height; y+=space) {
-      rect(x, y, 20, 40);
-        rect(x, y, 10, 40)
-        rect(x, y, 20, 20)
-        rect(x, y, 10, 20)
+function draw() {
+    background(220);
     
+    var windowWidth = width/6; // window width
+    var windowHeight = height/2; // window height
+    
+    for ( var x = windowWidth/4; x < width; x+=windowWidth) {
+       for (var y = windowHeight/4; y < height; y+=windowHeight) {
+           
+           
+           rect(x, y, windowWidth/2, windowHeight/2);
+           
+           fill("white");
+           stroke(167, 82, 54);
+           
+           rect(x, y, windowWidth/4, windowHeight/8);
+           rect(x, y + windowHeight/8, windowWidth/4, windowHeight/8);
+           rect(x + windowWidth/4, y + windowHeight/8, windowWidth/4, windowHeight/8);
+           rect(x + windowWidth/4, y + windowHeight/8 * 2, windowWidth/4, windowHeight/8);
+           
+           
+        }
     }
-  }
+        
 }
