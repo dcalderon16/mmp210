4d383dbd21bd5d59a5009e704169f66b91ce15c4
dcalderon16 <dcalderon14@nycboss.org>
2017-05-21 21:48:17 -0400

    Add files via upload

--- week6/sketch.js
+++ week6/sketch.js
@@ -1,12 +1,20 @@
 function setup() {
-  createCanvas(640, 480);
+    createCanvas(640, 480);
+   // frameRate(2);
 }
-
+ 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+    translate(random(0,width), random(0,height));
+ 
+    angleMode(DEGREES);
+    rotate(random(1, 360));
+    rect(400, 50, 50, 370);
+
+    triangle(400, 50, 425, 5, 450, 50);
+
+    rect(400, 400, 50, 50);
+
+    rect(400, 430, 50, 40);
+    
+    triangle(411, 30, 425, 5, 439, 30);
 }
